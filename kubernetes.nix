c7f25b0e0f85e0d289c8599724ee90bbe9f01965
Michel R Vaillancourt <michel@michelrvaillancourt.com>
2024-05-14 14:42:37 -0300

    37 current  2024-05-14 14:42:27  23.11.7093.44072e24566c  6.1.90                          *

--- kubernetes.nix
+++ kubernetes.nix
@@ -11,10 +11,10 @@
 }: let
   # When using easyCerts=true the IP Address must resolve to the master on creation.
   # So use simply 127.0.0.1 in that case. Otherwise you will have errors like this https://github.com/NixOS/nixpkgs/issues/59364
-  # kubeMasterIP = "127.0.0.1";
-  # kubeMasterHostname = "localhost";
-  kubeMasterIP = "10.1.1.10";
-  kubeMasterHostname = "kube_main_alpha";
+  kubeMasterIP = "127.0.0.1";
+  kubeMasterHostname = "localhost";
+  #kubeMasterIP = "10.1.1.10";
+  #kubeMasterHostname = "kube_main_alpha";
   kubeMasterAPIServerPort = 6443;
 in {
   networking = {
@@ -47,20 +47,20 @@ in {
     kubectl
     kubernetes
   ];
-  services.kubernetes = {
-    easyCerts = true;
-    roles = ["master"];
-    masterAddress = kubeMasterHostname;
-    apiserverAddress = "https://${kubeMasterHostname}:${toString kubeMasterAPIServerPort}";
-    apiserver = {
-      securePort = kubeMasterAPIServerPort;
-      advertiseAddress = kubeMasterIP;
-    };
-
-    # use coredns
-    #-    addons.dns.enable = true;
-
-    # needed if you use swap
-    #-    kubelet.extraOpts = "--fail-swap-on=false";
-  };
+  #-  services.kubernetes = {
+  #-    easyCerts = true;
+  #-    roles = ["master"];
+  #-    masterAddress = kubeMasterHostname;
+  #-    apiserverAddress = "https://${kubeMasterHostname}:${toString kubeMasterAPIServerPort}";
+  #-    apiserver = {
+  #-      securePort = kubeMasterAPIServerPort;
+  #-      advertiseAddress = kubeMasterIP;
+  #-    };
+  #-
+  #-    # use coredns
+  #-        addons.dns.enable = true;
+  #-
+  #-    # needed if you use swap
+  #-        kubelet.extraOpts = "--fail-swap-on=false";
+  #-  };
 }
