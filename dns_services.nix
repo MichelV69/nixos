77021dc6e52ee48fcc570be46dcb58abd6963dd7
Michel R Vaillancourt <michel@michelrvaillancourt.com>
2024-05-14 12:31:10 -0300

    32 current  2024-05-14 12:31:02  23.11.7093.44072e24566c  6.1.90                          *

--- dns_services.nix
+++ dns_services.nix
@@ -11,4 +11,20 @@
   environment.systemPackages = [
     pkgs.dnsmasq
   ];
+  services.dnsmasq = {
+    enable = true;
+    alwaysKeepRunning = false; #only restart on error
+    resolveLocalQueries = true;
+    settings = {
+      domain-needed = true;
+      dhcp-range = ["192.168.0.22,192.168.0.88"];
+      server = [
+        "8.8.8.8"
+        "8.8.4.4"
+      ];
+    };
+    extraConfig = "";
+  };
+
+  services.nixops-dns.dnsmasq = true;
 }
