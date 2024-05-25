46b394ac854aff7c5a44741e7e7def96ab3feb6c
Michel R Vaillancourt <michel@michelrvaillancourt.com>
2024-05-25 15:52:44 -0300

    61 current  2024-05-25 15:24:36  23.11.7237.46397778ef1f  6.1.91                          *

---
+++ users.nix
@@ -0,0 +1,56 @@
+# users.nix
+#
+# Define a user accounts
+#
+{
+  pkgs,
+  config,
+  ...
+}: let
+  common_packages = with pkgs; [
+    wget
+    screen
+    mailutils
+  ];
+in {
+  users.groups = {
+    staff = {};
+    developers = {};
+    sysadmins = {};
+  };
+
+  users.users = {
+    michelv69 = {
+      isNormalUser = true;
+      description = "SysAdmin MichelV69";
+      uid = 9902;
+      initialPassword = "changeMeRightNow!";
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDNS3lil5pZIcRbnyE97bqxFB55MtkvojJcytHS3hyZ4"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJJ0p9LYcnYnqIFNBezlJ8qq5PELUnunK0BcbcZXQ8l+"
+      ];
+      extraGroups = ["networkmanager" "wheel" "staff" "developers" "sysadmins"];
+      shell = pkgs.zsh;
+      packages = with pkgs;
+        [
+          alejandra
+        ]
+        ++ common_packages;
+    };
+    kat_wilson = {
+      isNormalUser = true;
+      description = "developer kat_wilson";
+      uid = 9903;
+      initialPassword = "changeMeRightNow!";
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDNS3lil5pZIcRbnyE97bqxFB55MtkvojJcytHS3hyZ4"
+      ];
+      extraGroups = ["staff" "developers"];
+      shell = pkgs.zsh;
+      packages = with pkgs;
+        [
+        ]
+        ++ common_packages;
+    };
+  };
+}
