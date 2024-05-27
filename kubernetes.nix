268c5e701d55faadb5f887be9d65e0a646e63296
Michel R Vaillancourt <michel@michelrvaillancourt.com>
2024-05-27 17:50:49 -0300

    4 current   2024-05-27 17:50:42  23.11.7313.9d29cd266ceb  6.1.91                          *

--- kubernetes.nix
+++ kubernetes.nix
@@ -11,13 +11,32 @@
 }: let
   # When using easyCerts=true the IP Address must resolve to the master on creation.
   # So use simply 127.0.0.1 in that case. Otherwise you will have errors like this https://github.com/NixOS/nixpkgs/issues/59364
+  domain = "7965stpeters.local";
+  zone = "kube";
+  ip_v4_block = "10.1.1";
+  ip_v4_node_ptr = 10;
+
   kubeBuildIP = "127.0.0.1";
   kubeBuildHostname = "localhost";
-
-  domain = "7965stpeters.local";
-  kubeMasterIP = "10.1.1.10";
-  kubeMasterHostname = "main_alpha.kube.${domain}";
   kubeMasterAPIServerPort = 6443;
+
+  kube_managers = {
+    alpha = {
+      name = "alpha.${zone}.${domain}";
+      ip_v4 = "${ip_v4_block}.${toString ip_v4_node_ptr}";
+    };
+    ip_v4_node_ptr = ip_v4_node_ptr + 1;
+    bravo = {
+      name = "bravo.${zone}.${domain}";
+      ip_v4 = "${ip_v4_block}.${toString ip_v4_node_ptr}";
+    };
+  };
+
+  kube_workers = {
+    alpha = {
+      name = "alpha";
+    };
+  };
 in {
   networking = {
     vlans = {
@@ -28,16 +47,16 @@ in {
     };
     interfaces.vlan100.ipv4.addresses = [
       {
-        address = "10.1.1.1";
+        address = "${ip_v4_block}.1";
         prefixLength = 24;
       }
       {
-        address = "${kubeMasterIP}";
+        address = "${kube_managers.alpha.ip_v4}";
         prefixLength = 24;
       }
     ];
     hosts = {
-      "${kubeMasterIP}" = ["${kubeMasterHostname}"];
+      "${kube_managers.alpha.ip_v4}" = ["${kube_managers.alpha.name}"];
       "10.1.1.11" = ["node-alpha.kube.${domain}"];
       "10.1.1.12" = ["node-bravo.kube.${domain}"];
     };
@@ -56,7 +75,7 @@ in {
     apiserverAddress = "https://${kubeBuildHostname}:${toString kubeMasterAPIServerPort}";
     apiserver = {
       securePort = kubeMasterAPIServerPort;
-      advertiseAddress = kubeMasterIP;
+      advertiseAddress = kube_managers.alpha.ip_v4;
     };
     # use coredns
     addons.dns.enable = true;
