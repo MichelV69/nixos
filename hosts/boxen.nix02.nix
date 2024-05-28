dacf2b102e6d506fca326642246f97a70e2ae72c
Michel R Vaillancourt <michel@michelrvaillancourt.com>
2024-05-28 13:39:04 -0300

    Add new file to repo

---
+++ hosts/boxen.nix02.nix
@@ -0,0 +1,33 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  # global relam options used outside this file
+  StPeters7965.X11Forwarding = true;
+  StPeters7965.kubeRole = "node";
+
+  # other box specific options we can just set here
+  # Set your time zone.
+  time.timeZone = "America/Halifax";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_CA.UTF-8";
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  networking = {
+    hostName = "nix02"; # Define your hostname.
+    dhcpcd.enable = false;
+    interfaces.enp1s0.ipv4.addresses = [
+      {
+        address = "192.168.0.114";
+        prefixLength = 24;
+      }
+    ];
+  };
+}
