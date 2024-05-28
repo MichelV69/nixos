34e6f78769c1deaee9c67f89aa8f595aee877180
Michel R Vaillancourt <michel@michelrvaillancourt.com>
2024-05-28 12:50:52 -0300

    9 current   2024-05-28 12:34:56  23.11.7313.9d29cd266ceb  6.1.91                          *

---
+++ configuration.nix
@@ -0,0 +1,59 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+{
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    "${builtins.fetchTarball "https://github.com/nix-community/disko/archive/master.tar.gz"}/module.nix"
+    ./disk-config.nix
+    ./realm_library.nix
+    ./perHost.nix
+    ./addSwapFile.nix
+    ./net.7965StPeters.local.nix
+    ./wifi.nix
+    ./firewall.nix
+    ./ssh.nix
+    ./dns_services.nix
+    ./kubernetes.nix
+    ./janitor.nix
+    ./users.nix
+  ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  environment.systemPackages = with pkgs; [
+    pkgs.nix-ld
+    pkgs.nix-output-monitor
+    git
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken.
+  system.stateVersion = "23.11";
+
+  programs.nix-ld.enable = true;
+  programs.zsh.enable = true;
+}
--- hosts/boxen.nix01.nix
+++ hosts/boxen.nix01.nix
@@ -1,35 +1,12 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
 {
   config,
   pkgs,
   ...
 }: {
-  imports = [
-    # Include the results of the hardware scan.
-    ./hardware-configuration.nix
-    "${builtins.fetchTarball "https://github.com/nix-community/disko/archive/master.tar.gz"}/module.nix"
-    ./disk-config.nix
-    ./systems_library.nix
-    ./addSwapFile.nix
-    ./net.7965StPeters.local.nix
-    ./wifi.nix
-    ./firewall.nix
-    ./ssh.nix
-    ./dns_services.nix
-    ./kubernetes.nix
-    ./janitor.nix
-    ./users.nix
-  ];
+  # global relam options used outside this file
+  StPeters7965.booleanTest = true;
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
+  # other box specific options we can just set here
   # Set your time zone.
   time.timeZone = "America/Halifax";
 
@@ -42,37 +19,6 @@
     xkbVariant = "";
   };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  environment.systemPackages = with pkgs; [
-    pkgs.nix-ld
-    pkgs.nix-output-monitor
-    git
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  programs.mtr.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-  };
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
-
-  # --- going beyond defaults ---
-  programs.nix-ld.enable = true;
-  programs.zsh.enable = true;
-
-  StPeters7965.booleanTest = true;
   networking = {
     hostName = "nix01"; # Define your hostname.
     dhcpcd.enable = false;
