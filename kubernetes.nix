5f3f578e886215ea33cfd924ca38dda6e70835bc
SysAdmin MichelV69 <902pe_gaming@wolfstar.ca>
2024-06-13 09:50:42 -0300

    Gen 10 09:50:16

--- kubernetes.nix
+++ kubernetes.nix
@@ -9,8 +9,10 @@
   config,
   ...
 }: let
-  # When using easyCerts=true the IP Address must resolve to the master on creation.
-  # So use simply 127.0.0.1 in that case. Otherwise you will have errors like this https://github.com/NixOS/nixpkgs/issues/59364
+  # When using easyCerts=true the IP Address must resolve to
+  # 	the master on creation.
+  # So use simply 127.0.0.1 in that case. Otherwise you will have
+  #	errors like this https://github.com/NixOS/nixpkgs/issues/59364
   realmCfg = config.StPeters7965;
   zone = "kube";
   ip_v4_block = "192.168.0";
