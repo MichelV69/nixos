dc38b4660b2468fb68c58acd46ded304ad9f6383
SysAdmin MichelV69 <902pe_gaming@wolfstar.ca>
2024-06-13 16:37:26 -0300

    Gen 27 16:10:59

--- kubernetes.nix
+++ kubernetes.nix
@@ -5,18 +5,62 @@
 # for kubernetes
 #
 {
-  config,
   pkgs,
+  config,
   ...
 }: let
-  # When using easyCerts=true the IP Address must resolve to the master on creation.
-  # So use simply 127.0.0.1 in that case. Otherwise you will have errors like this https://github.com/NixOS/nixpkgs/issues/59364
-  kubeMasterIP = "10.1.1.2";
-  kubeMasterHostname = "localhost";
+  realmCfg = config.StPeters7965;
+  zone = "kube";
+  ip_v4_block = "192.168.0";
+  kubeBuildIP = "127.0.0.1";
+  kubeBuildHostname = "localhost";
   kubeMasterAPIServerPort = 6443;
+
+  kube_managers = {
+    alpha = {
+      name = "alpha_mgr.${zone}.${realmCfg.domain}";
+      ip_v4 = "${ip_v4_block}.113";
+    };
+    bravo = {
+      name = "bravo_mgr.${zone}.${realmCfg.domain}";
+      ip_v4 = "${ip_v4_block}.114";
+    };
+  };
+
+  kube_workers = {
+    alpha = {
+      name = "alpha_wrk.${zone}.${realmCfg.domain}";
+      ip_v4 = "${ip_v4_block}.120";
+    };
+    bravo = {
+      name = "bravo_wrk.${zone}.${realmCfg.domain}";
+      ip_v4 = "${ip_v4_block}.121";
+    };
+    charlie = {
+      name = "charlie_wrk.${zone}.${realmCfg.domain}";
+      ip_v4 = "${ip_v4_block}.122";
+    };
+  };
+
+  realManagerIP = kubeBuildIP;
+  #  if ("${realmCfg.kubeRole}" != "node")
+  #  then kube_managers.alpha.ip_v4
+  #  else kubeBuildIP;
+
+  realManagerName = kubeBuildHostname;
+  #  if ("${realmCfg.kubeRole}" != "node")
+  #  then kube_managers.alpha.name
+  #  else kubeBuildHostname;
 in {
-  # resolve master hostname
-  networking.extraHosts = "${kubeMasterIP} ${kubeMasterHostname}";
+  networking = {
+    hosts = {
+      "${kube_managers.alpha.ip_v4}" = ["${kube_managers.alpha.name}"];
+      "${kube_managers.bravo.ip_v4}" = ["${kube_managers.bravo.name}"];
+      "${kube_workers.alpha.ip_v4}" = ["${kube_workers.alpha.name}"];
+      "${kube_workers.bravo.ip_v4}" = ["${kube_workers.bravo.name}"];
+      "${kube_workers.charlie.ip_v4}" = ["${kube_workers.charlie.name}"];
+    };
+  };
 
   # packages for administration tasks
   environment.systemPackages = with pkgs; [
@@ -24,20 +68,17 @@ in {
     kubectl
     kubernetes
   ];
-
   services.kubernetes = {
-    roles = ["master" "node"];
-    masterAddress = kubeMasterHostname;
-    apiserverAddress = "https://${kubeMasterHostname}:${toString kubeMasterAPIServerPort}";
     easyCerts = true;
+    roles = ["${realmCfg.kubeRole}"];
+    masterAddress = realManagerIP;
+    apiserverAddress = "https://${realManagerName}:${toString kubeMasterAPIServerPort}";
     apiserver = {
       securePort = kubeMasterAPIServerPort;
-      advertiseAddress = kubeMasterIP;
+      advertiseAddress = realManagerIP;
     };
-
     # use coredns
     addons.dns.enable = true;
-
     # needed if you use swap
     kubelet.extraOpts = "--fail-swap-on=false";
   };
