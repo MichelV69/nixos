d0d6c90af62b54df669cd2df23b15a2e288d709e
SysAdmin MichelV69 <902pe_gaming@wolfstar.ca>
2024-06-16 20:37:24 -0300

    Gen 40 20.37.17

--- firewall.nix
+++ firewall.nix
@@ -11,12 +11,13 @@
   tcp_ssh = 22;
   tcp_kubctl = 6443;
   tcp_kubapi = 8080;
+  tcp_kublet_reg = 8888;
   tcp_dnsmasq_dns = 53;
   tcp_dnsmasq_dhcp = 76;
 in {
   networking = {
     firewall.enable = true;
-    firewall.allowedTCPPorts = [tcp_ssh tcp_kubctl tcp_kubapi tcp_dnsmasq_dns];
+    firewall.allowedTCPPorts = [tcp_ssh tcp_kubctl tcp_kubapi tcp_dnsmasq_dns tcp_kublet_reg];
     firewall.allowedUDPPorts = [tcp_dnsmasq_dhcp];
   };
 }
