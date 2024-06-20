8ffb7d226dc6ed7868d96ed60817e744e68e57ae
SysAdmin MichelV69 <902pe_gaming@wolfstar.ca>
2024-06-20 11:02:53 -0300

    Gen 42 11.02.48

--- users.nix
+++ users.nix
@@ -13,29 +13,6 @@
     stow
     wget
   ];
-  #  vim_packages = with pkgs; [
-  #    neovim
-  #    vimPlugins.barbecue-nvim
-  #    vimPlugins.zoxide-vim
-  #    vimPlugins.vim-toml
-  #    vimPlugins.vim-ruby
-  #    vimPlugins.vim-rails
-  #    vimPlugins.vim-nix
-  #    vimPlugins.vim-addon-nix
-  #    vimPlugins.vim-markdown
-  #    vimPlugins.vim-lua
-  #    vimPlugins.vim-ledger
-  #    vimPlugins.vim-fugitive
-  #    vimPlugins.vim-crates
-  #    vimPlugins.vim-colorschemes
-  #    vimPlugins.vim-addon-manager
-  #    vimPlugins.vim-addon-completion
-  #    vimPlugins.todo-txt-vim
-  #    vimPlugins.todo-comments-nvim
-  #    vimPlugins.tagbar
-  #    vimPlugins.tabline-nvim
-  #    vimPlugins.vim-airline
-  #  ];
 in {
   users.groups = {
     staff = {};
@@ -63,6 +40,7 @@ in {
           zoxide
           gh
           micro
+          neovim
         ]
         ++ common_packages;
     };
