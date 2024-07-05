e678985fd6adb9c5a749b36134159461d411cbd9
SysAdmin MichelV69 <902pe_gaming@wolfstar.ca>
2024-07-05 11:18:16 -0300

    Gen 56 11.18.08

--- firewall.nix
+++ firewall.nix
@@ -14,10 +14,11 @@
   tcp_kublet_reg = 8888;
   tcp_dnsmasq_dns = 53;
   tcp_dnsmasq_dhcp = 76;
+  tcp_rust_rocket_app = 8000;
 in {
   networking = {
     firewall.enable = true;
-    firewall.allowedTCPPorts = [tcp_ssh tcp_kubctl tcp_kubapi tcp_dnsmasq_dns tcp_kublet_reg];
+    firewall.allowedTCPPorts = [tcp_ssh tcp_kubctl tcp_kubapi tcp_dnsmasq_dns tcp_kublet_reg tcp_rust_rocket_app];
     firewall.allowedUDPPorts = [tcp_dnsmasq_dhcp];
   };
 }
