2f77182f1c1f32cd490d46fa6fd84fc704d6dad1
SysAdmin MichelV69 <902pe_gaming@wolfstar.ca>
2024-07-08 14:06:12 -0300

    Gen 84 14.06.07

--- hosts/boxen.nix01.nix
+++ hosts/boxen.nix01.nix
@@ -51,7 +51,7 @@ in {
                  }
 
               upstream rust_rocket1 {
-                server 127.0.0.1:8000;
+                server 127.0.0.1:8001;
               }
               server {
                 listen ''
