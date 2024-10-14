71c3439672a6e64424b8ebe87da844dcc9f3ae36
SysAdmin MichelV69 <902pe_gaming@wolfstar.ca>
2024-10-14 11:16:57 -0300

    Gen 30 11.16.51

--- hosts/boxen.nix114.nix
+++ hosts/boxen.nix114.nix
@@ -29,6 +29,8 @@ in {
   StPeters7965.k3sCfg.ip_v4_mask = k3s_ip_v4_mask;
   StPeters7965.k3sCfg.my4xIP = k3s_my4xIP;
 
+  virtualisation.docker.enable = true;
+
   # --- unlikely to need to change below ---
   # Set your time zone.
   time.timeZone = "America/Halifax";
